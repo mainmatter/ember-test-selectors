15865a3baa5d213f440e0b87e332804c577f1bd6
Marine Dunstetter <bluecut.official@gmail.com>
2025-03-26 15:49:15 +0100

    clean-up package.json and lint

--- ember-test-selectors/.eslintrc.js
+++ ember-test-selectors/.eslintrc.js
@@ -8,36 +8,13 @@ module.exports = {
   overrides: [
     // node files
     {
-      files: [
-        '**/.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js',
-      ],
-      excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
+      files: ['**/.eslintrc.js', 'index.js', 'config/**/*.js'],
       parserOptions: {
         sourceType: 'script',
         ecmaVersion: 2015,
       },
       env: {
         browser: false,
-        node: true,
-      },
-    },
-    // node test files
-    {
-      files: ['node-tests/*.js'],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015,
-      },
-      env: {
-        browser: false,
-        mocha: true,
         node: true,
       },
     },
